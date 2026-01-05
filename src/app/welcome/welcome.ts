bbe9c4b9cf38cbbd23370c1c1bd7dad6a6936a23
Muhammad Salman <connect.msalman@gmail.com>
2026-01-05 15:44:10 +0500

    initialize .postcssrc.json

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './welcome.css',
 })
 export class Welcome {
-
+  title : string = 'Welcome to Angular Learnings';
+  subtitle : string = 'Learning Angular to strengthen my frontend skills.';
+  description : string = 'A collection of insights, tips, and practical experiences gained while working with Angular, a popular front-end framework. This resource covers essential concepts like components, modules, services, routing, and data binding, along with best practices for building scalable and maintainable web applications. Ideal for beginners and intermediate developers looking to strengthen their Angular skills.';
 }
